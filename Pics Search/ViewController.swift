980ae548d32b880caef243879edda9d0b4a355f7
semjonG <semjon.gerasimov@gmail.com>
2021-12-06 22:42:25 +0300

    Initial Commit

---
+++ Pics Search/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Pics Search
+//
+//  Created by mac on 06.12.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
