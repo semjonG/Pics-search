7bb8f8821924c4560ec736d5a4bd613fccb69540
semjonG <semjon.gerasimov@gmail.com>
2021-12-07 22:11:40 +0300

    full working app

--- Pics Search/ViewController.swift
+++ Pics Search/ViewController.swift
@@ -19,35 +19,87 @@ struct Result: Codable {
 }
 
 struct URLS: Codable {
-    let full: String
+    let regular: String
 }
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UICollectionViewDataSource, UISearchBarDelegate {
     
-    let urlString = "https://api.unsplash.com/search/photos?page=1&query=office&client_id=0jNpDtsjteqxueEK3azU6s9ePCu9ioo5M4LUPcRt0Vw"
+    private var collectionView: UICollectionView?
+    
+    var results: [Result] = []
+    
+    let searchBar = UISearchBar()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        fetchPhotos()
+        searchBar.delegate = self
+        view.addSubview(searchBar)
+        let layout = UICollectionViewFlowLayout()
+        layout.scrollDirection = .vertical
+        layout.minimumLineSpacing = 0
+        layout.minimumInteritemSpacing = 0
+        layout.itemSize = CGSize(width: view.frame.size.width/2, height: view.frame.size.width/2)
+        let collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
+        collectionView.register(ImageCollectionViewCell.self, forCellWithReuseIdentifier: ImageCollectionViewCell.identifier)
+        collectionView.dataSource = self
+        view.addSubview(collectionView)
+        collectionView.backgroundColor = .systemBackground
+        self.collectionView = collectionView
     }
     
-    func fetchPhotos () {
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        searchBar.frame = CGRect(x: 10, y: view.safeAreaInsets.top, width: view.frame.size.width-20, height: 50)
+        collectionView?.frame = CGRect(x: 0, y: view.safeAreaInsets.top+55, width: view.frame.size.width, height: view.frame.size.height-55)
+    }
+    
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        searchBar.resignFirstResponder()
+        if let text = searchBar.text {
+            results = []
+            collectionView?.reloadData()
+            fetchPhotos(query: text)
+        }
+    }
+    
+    func fetchPhotos (query: String) {
+        let urlString = "https://api.unsplash.com/search/photos?page=1&per_page=30&query=\(query)&client_id=0jNpDtsjteqxueEK3azU6s9ePCu9ioo5M4LUPcRt0Vw"
         guard let url = URL(string: urlString) else {
             return
         }
-        let task = URLSession.shared.dataTask(with: url) { data, _ , error in
+        let task = URLSession.shared.dataTask(with: url) { [weak self] data, _, error in
             guard let data = data, error == nil else {
                 return
             }
             do {
-                let jsonResults = try JSONDecoder().decode(APIResponse.self, from: data)
-                print(jsonResults.results.count)
+                let jsonResult = try JSONDecoder().decode(APIResponse.self, from: data)
+                DispatchQueue.main.async {
+                    self?.results = jsonResult.results
+                    self?.collectionView?.reloadData()
+                }
             }
             catch {
-                print(error )
+                print(error)
             }
         }
         task.resume()
     }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return results.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let imageURLString = results[indexPath.row].urls.regular
+        guard let cell = collectionView.dequeueReusableCell(
+            withReuseIdentifier: ImageCollectionViewCell.identifier,
+            for: indexPath
+        ) as? ImageCollectionViewCell else {
+            return UICollectionViewCell()
+        }
+        cell.configure(with: imageURLString)
+        return cell
+    }
 }
+
 
